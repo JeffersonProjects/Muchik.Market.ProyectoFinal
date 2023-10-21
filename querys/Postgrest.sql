20bf174e114fddafa849ff809fcf3501817426f2
caiquipa1206 <caiquipa1206@outlook.com>
2023-10-21 17:35:17 -0500

    cambios finales para presentacion.

--- querys/Postgrest.sql
+++ querys/Postgrest.sql
@@ -6,3 +6,6 @@ CREATE TABLE "invoice" (
 	"state" INTEGER NULL,
 	PRIMARY KEY ("id_invoice")
 )
+insert into invoice (amount, state) values (500, 0);
+insert into invoice (amount, state) values (1500, 0);
+insert into invoice (amount, state) values (6850, 0)
